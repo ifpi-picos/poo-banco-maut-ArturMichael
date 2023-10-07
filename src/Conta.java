d900d96e4f2212e9382e13d738cffb65e89decff
ArturMichael <arturalvesmorais8@gmail.com>
2023-10-07 17:28:39 -0300

    Declarando atributos

---
+++ src/Conta.java
@@ -0,0 +1,14 @@
+public class Conta {
+    private String numeroAgencia;
+    private String numeroConta;
+    private  double Saldo;
+    
+    
+
+    public Conta(String numeroAgencia, String numeroConta, double Saldo) {
+        this.numeroAgencia = numeroAgencia;
+        this.numeroConta = numeroConta;
+        this.Saldo = 0.0;
+    
+    }
+}
