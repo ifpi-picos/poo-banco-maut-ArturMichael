8ed8291734e811c1904c63765d447968cea4b385
ArturMichael <arturalvesmorais8@gmail.com>
2023-10-07 18:18:35 -0300

    Implementendo métodos e fazendo correções

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,14 +5,39 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String dataNascimento;
-    private String endereço;
     private List<Conta> contas;
 
-    public Cliente(String nome, String cpf, String dataNascimento, String endereço) {
+    public Cliente(String nome, String cpf, String dataNascimento) {
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
-        this.endereço = endereço;
         this.contas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void abrirConta(Conta conta) {
+        contas.add(conta);
     }
 }
