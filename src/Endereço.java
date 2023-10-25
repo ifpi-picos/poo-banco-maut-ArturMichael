d29b3c1f535417c6f7b2e4370209f40a6240a1cb
ArturMichael <arturalvesmorais8@gmail.com>
2023-10-25 10:54:51 -0300

    adicionando atributo e metodo

--- src/Endereço.java
+++ src/Endereço.java
@@ -1,66 +1,59 @@
 public class Endereço {
     private String rua;
-    private String numero;
     private String bairro;
     private String cidade;
     private String estado;
     private String cep;
-
-    public Endereço(String rua, String numero, String bairro, String cidade, String estado, String cep) {
+    
+    public Endereço(String rua, String bairro, String cidade, String estado, String cep) {
         this.rua = rua;
-        this.numero = numero;
         this.bairro = bairro;
         this.cidade = cidade;
         this.estado = estado;
+        this.cep = cep;
     }
 
-     // Getters e Setters
-     public String getRua() {
-        return rua;
+    public Endereço(String numero, String cep2) {
     }
-    
+
     public void setRua(String rua) {
         this.rua = rua;
     }
-    
-    public String getNumero() {
-        return numero;
-    }
-    
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-    
-    public String getBairro() {
-        return bairro;
-    }
-    
+
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
-    
+
     public String getCidade() {
         return cidade;
     }
-    
+
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
-    
+
     public String getEstado() {
         return estado;
     }
-    
+
     public void setEstado(String estado) {
         this.estado = estado;
     }
-    
+
     public String getCep() {
         return cep;
     }
-    
+
     public void setCep(String cep) {
         this.cep = cep;
     }
 
+    @Override  
+    public String toString() {
+        return "Endereco [bairro=" + bairro + ", cep=" + cep + ", cidade=" + cidade + ", estado=" + estado + ", rua="
+                + rua + "]";
+    }
+
+    
+    
 }
