eabf6ff9787eed7135c8343d973f04aec79f794d
ArturMichael <arturalvesmorais8@gmail.com>
2023-10-25 08:32:39 -0300

    adicionando metodos e atributos

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,12 +5,14 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String dataNascimento;
+    private Endereço endereco;
     private List<Conta> contas;
 
-    public Cliente(String nome, String cpf, String dataNascimento) {
+    public Cliente(String nome, String cpf, String dataNascimento,  Endereço endereco) {
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
         this.contas = new ArrayList<>();
     }
 
@@ -26,18 +28,39 @@ public class Cliente {
         return cpf;
     }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public String getDataNascimento() {
         return dataNascimento;
     }
+
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
+    }
+
+    public Endereço getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereço endereco) {
+        this.endereco = endereco;
     }
 
     public List<Conta> getContas() {
         return contas;
     }
 
-    public void abrirConta(Conta conta) {
-        contas.add(conta);
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
     }
+
+    public Cliente() {
+        this.nome = "";
+        this.cpf = "";
+        this.dataNascimento = null;
+        this.endereco = null;
+    }
+    
 }
