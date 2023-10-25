4d9f22843cb10dab87801db89a301be81bf75e52
ArturMichael <arturalvesmorais8@gmail.com>
2023-10-25 18:47:02 -0300

    classe contaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,20 +2,36 @@ public class ContaCorrente extends Conta {
     private double chequeEspecial;
     private int transferenciasRealizadas;
 
-    public ContaCorrente(String agencia, String numero, Cliente cliente, double chequeEspecial, int transferenciasRealizadas, Notificacoes notificacoes) {
-        super(agencia, numero, cliente, transferenciasRealizadas, notificacoes);
-        this.chequeEspecial = chequeEspecial;
+    public ContaCorrente(String agencia, String numero, Cliente cliente, Notificacoes notificacoes) {
+        super(agencia, numero, cliente, notificacoes);
+        this.chequeEspecial = 1000;
         this.transferenciasRealizadas = 0;
     }
 
     @Override
     public void transferir(Conta contaDestino, double valor) {
-        if (transferenciasRealizadas < 2) {
-            super.transferir(contaDestino, valor);
+        if (valor <= saldo) {
+            if(transferenciasRealizadas >2){
+            this.saldo -= valor+(valor *0.1);
+            contaDestino.saldo +=valor;
+            notificacoes.enviaNotificacoes("transferencia", valor);
+            getTransacoes().add(new Transacoes("trasferencia", valor));
+            contaDestino.getTransacoes().add(new Transacoes("recibo transferencia", valor));
             transferenciasRealizadas++;
         } else {
-            double taxa = valor * 0.10;
-            super.transferir(contaDestino, valor + taxa);
+            this.saldo -= valor;
+            contaDestino.saldo +=valor;
+            notificacoes.enviaNotificacoes("transferencia", valor);
+            getTransacoes().add(new Transacoes("trasferencia", valor));
+            contaDestino.getTransacoes().add(new Transacoes("recibo transferencia", valor));
+            transferenciasRealizadas++;
+        }
+    }
+
+        if (notificacoes != null) {
+            notificacoes.enviaNotificacoes("deposito" , valor);
+        }else {
+            System.out.println("Nenhuma notificacao");
         }
     }
 
@@ -38,6 +54,27 @@ public class ContaCorrente extends Conta {
     public void notificacoes() {
     }
 
+    @Override
+    public void sacar(double valor) {
+        if (valor <= saldo + chequeEspecial) {
+            this.saldo -= valor;
+            getTransacoes().add(new Transacoes("saque", valor));
+            notificacoes.enviaNotificacoes("deposito" , valor);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+
+        if (notificacoes != null) {
+            notificacoes.enviaNotificacoes("deposito" , valor);
+        }else {
+            System.out.println("Nenhuma notificacao");
+        }
+    }
+
     
 }
-
